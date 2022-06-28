e62cb2922238cf731ff2078bd651b432ca32079a
EmanueleEsposito <manu2022@blu.it>
2022-06-28 17:13:23 +0200

    aggiunti try e catch nel main e fine esercizio

--- src/jana60/poligoni/Main.java
+++ src/jana60/poligoni/Main.java
@@ -3,13 +3,22 @@ package jana60.poligoni;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		try {
+			Poligono quadrato = new Quadrato(10);
+			quadrato.calcolaPerimetro();
+			quadrato.calcolaArea();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 
-		Poligono quadrato = new Quadrato(10);
-		quadrato.calcolaPerimetro();
-		quadrato.calcolaArea();
-		Poligono rettangolo = new Rettangolo(5, 27);
-		rettangolo.calcolaPerimetro();
-		rettangolo.calcolaArea();
+		try {
+			Poligono rettangolo = new Rettangolo(5, 27);
+			rettangolo.calcolaPerimetro();
+			rettangolo.calcolaArea();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 
 	}
 
